6113be2fffc470b917c4721b62c85aad85cb282d
André Ferreira <contato@andreferreira.me>
2017-03-06 02:37:43 -0300

    First commit

--- HelloXamarinFormsSAP/HelloXamarinFormsSAP/App.cs
+++ HelloXamarinFormsSAP/HelloXamarinFormsSAP/App.cs
@@ -7,37 +7,49 @@ using Xamarin.Forms;
 
 namespace HelloXamarinFormsSAP
 {
-	public class App : Application
-	{
-		public App ()
-		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            #if __IOS__
+                                Text = "Welcome to iOS Xamarin Forms!"
+                            #elif __ANDROID__
+                                Text = "Welcome to Android Xamarin Forms!"
+                            #elif WINDOWS_UWP
+                                Text = "Welcome to Windows UWP Xamarin Forms!"
+                            #elif WINDOWS_APP
+                                Text = "Welcome to Windows Xamarin Forms!"
+                            #elif WINDOWS_PHONE_APP
+                                Text = "Welcome to Windows Phone Xamarin Forms!"
+                            #endif
 						}
-					}
-				}
-			};
-		}
+                    }
+                }
+            };
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
